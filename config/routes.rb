e137109948c7f6508acb7c70ec8e05ab6e2b4f9c
23nith <user.name>
2022-05-04 03:43:29 +0800

    added pages-home, navbar and partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "categories#index"
+  # root "categories#index"
 
   get '/categories' => 'categories#index'
 
